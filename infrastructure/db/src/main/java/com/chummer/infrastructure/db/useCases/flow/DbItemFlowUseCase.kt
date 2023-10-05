fde8ffd954b85aeb538720bd4321f2870e062aee
Herman <chummer1008@gmail.com>
2023-10-06 00:13:24 +0300

    Switch to invoke operators

--- infrastructure/db/src/main/java/com/chummer/infrastructure/db/useCases/flow/DbItemFlowUseCase.kt
+++ infrastructure/db/src/main/java/com/chummer/infrastructure/db/useCases/flow/DbItemFlowUseCase.kt
@@ -5,13 +5,17 @@ import app.cash.sqldelight.coroutines.asFlow
 import app.cash.sqldelight.coroutines.mapToOne
 import com.chummer.infrastructure.db.query.HasQuery
 import com.chummer.infrastructure.usecase.FlowUseCase
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlin.coroutines.CoroutineContext
 import app.cash.sqldelight.Query as SqlQuery
 
 abstract class DbItemFlowUseCase<QueryArgument, Row : Any, QueryTransacter : Transacter>(
     id: String,
     protected val transacter: QueryTransacter
 ) : FlowUseCase<QueryArgument, Row>(id), HasQuery<QueryArgument, Row, QueryTransacter> {
+
+    override val coroutineContext: CoroutineContext = Dispatchers.IO
 
     override fun invoke(input: QueryArgument): Flow<Row> {
         return transacter.getQuery(input).toFlow()
