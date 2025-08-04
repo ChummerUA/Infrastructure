27cd46fda4b4418a03a0c86a31badd24d7164f58
Herman <chummer1008@gmail.com>
2025-08-04 11:37:48 +0300

    Fix insert with result

--- infrastructure/db/src/main/java/com/chummer/infrastructure/db/useCases/insert/withResult/DbInsertUseCase.kt
+++ infrastructure/db/src/main/java/com/chummer/infrastructure/db/useCases/insert/withResult/DbInsertUseCase.kt
@@ -1,7 +1,7 @@
 package com.chummer.infrastructure.db.useCases.insert.withResult
 
+import app.cash.sqldelight.ExecutableQuery
 import app.cash.sqldelight.Transacter
-import app.cash.sqldelight.db.QueryResult
 import com.chummer.infrastructure.db.useCases.DbExecutableUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlin.coroutines.CoroutineContext
@@ -14,8 +14,8 @@ abstract class DbInsertUseCase<Row : Any, Result: Any, QueryTransacter : Transac
     override val coroutineContext: CoroutineContext = Dispatchers.IO
 
     override suspend fun execute(input: Row): Result {
-        return transacter.insert(input).value
+        return transacter.insert(input).executeAsOne()
     }
 
-    protected abstract fun QueryTransacter.insert(item: Row): QueryResult<Result>
+    protected abstract fun QueryTransacter.insert(item: Row): ExecutableQuery<Result>
 }
